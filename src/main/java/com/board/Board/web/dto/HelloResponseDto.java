11acba9cbb6b67f83a770095965bc28d87f19660
RY <45328298+rldyfh@users.noreply.github.com>
2021-06-07 15:57:08 +0900

    HelloController

---
+++ src/main/java/com/board/Board/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.board.Board.web.dto;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
